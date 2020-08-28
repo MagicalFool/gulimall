2a1ed8db9047e0e77bfd04dfefb68fd9a38897dc
chenchao <571621652@qq.com>
2020-08-29 06:06:07 +0800

    git init

---
+++ gulimall-coupon/src/test/java/com/cc/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
